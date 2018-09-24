43840f670f520db6879551f2270c7857b884e222
Chase Brewer <chase.brewer@dunbarsecured.com>
2018-09-24 16:02:47 -0400

    Add ability to lock currently active nessus scans to prevent duplicates
    There were instances when a write command from fsnotify was triggered multiple times for the same scan UUID, causing the same scan to be sent multiple times. This adds a concurrently safe list of current nessus scans that keeps track of the currently in process scans to prevent duplicate scans from occurring.
    
    Also broke down some of the functionality into separate functions with extra comments to make the code a little more readable.

--- beater/nessusbeat.go
+++ beater/nessusbeat.go
@@ -18,70 +18,81 @@ import (
 	"github.com/fsnotify/fsnotify"
 
 	"github.com/dunbarcyber/nessusbeat/config"
+	"sync"
 )
 
 type Nessusbeat struct {
-	done   chan struct{}
-	config config.Config
-	client beat.Client
+	done   chan struct{}   // Channel that closes when the beat was signaled to close.
+	config config.Config   // Parsed configuration file options.
+	scans  map[string]bool // Map of the current ongoing scans.
+	name   string          // Configured beat name retrieved from b.Info.Name.
+	mutex  sync.Mutex      // Locking mutex to prevent concurrent writes to scans map.
+	events chan beat.Event // Channel containing the parsed nessus scan events.
+	client beat.Client     //
 }
 
 func New(b *beat.Beat, cfg *common.Config) (beat.Beater, error) {
-	config := config.DefaultConfig
-	if err := cfg.Unpack(&config); err != nil {
-		return nil, fmt.Errorf("Error reading config file: %v", err)
+	conf := config.DefaultConfig
+	if err := cfg.Unpack(&conf); err != nil {
+		return nil, fmt.Errorf("error reading config file: %v", err)
 	}
-
-	bt := &Nessusbeat{
-		done:   make(chan struct{}),
-		config: config,
-	}
-	return bt, nil
+	return &Nessusbeat{config: conf}, nil
 }
 
-func (bt *Nessusbeat) ScanIDByUUID(nessus nessie.Nessus, uuid string) (int64, error) {
+// Connects to the nessus REST API.
+func connectAPI(url, cert string) (nessie.Nessus, error) {
+	switch cert {
+	case "":
+		return nessie.NewInsecureNessus(url)
+	default:
+		return nessie.NewNessus(url, cert)
+	}
+}
+
+// Retrieves the scan ID by it's UUID.
+func scanID(nessus nessie.Nessus, uuid string) (int64, error) {
 	result, err := nessus.Scans()
 	if err != nil {
-		return -1, err
+		return 0, err
 	}
 	for _, scan := range result.Scans {
 		if scan.UUID == uuid {
 			return scan.ID, nil
 		}
 	}
-	return -1, nil
+	return 0, nil
 }
 
-func (bt *Nessusbeat) NewConnection() (*nessie.Nessus, error) {
-	var (
-		nessus nessie.Nessus
-		err    error
-	)
-	if bt.config.CaCertPath != "" {
-		nessus, err = nessie.NewNessus(bt.config.NessusApiUrl, bt.config.CaCertPath)
-	} else {
-		nessus, err = nessie.NewInsecureNessus(bt.config.NessusApiUrl)
-	}
-	return &nessus, err
-}
-
-func (bt *Nessusbeat) Login(nessus nessie.Nessus) error {
-	return nessus.Login(bt.config.NessusApiUsername, bt.config.NessusApiPassword)
-}
-
-func (bt *Nessusbeat) ExportScanCSV(nessus nessie.Nessus, uuid string) ([]byte, error) {
-	scanID, err := bt.ScanIDByUUID(nessus, uuid)
+// Creates a watcher to watch a specified path.
+func watch(path string) (*fsnotify.Watcher, error) {
+	watcher, err := fsnotify.NewWatcher()
 	if err != nil {
-		return []byte{}, err
+		return nil, fmt.Errorf("cannot create watcher: %s", err)
+	}
+	err = watcher.Add(path)
+	if err != nil {
+		return nil, fmt.Errorf("cannot watch path '%s': %s", path, err)
+	}
+	return watcher, nil
+}
+
+// Downloads a nessus scan matching the given UUID.
+func exportCSVScan(nessus nessie.Nessus, uuid string) ([]byte, error) {
+	scanID, err := scanID(nessus, uuid)
+	if err != nil {
+		return nil, err
 	}
 	exportID, err := nessus.ExportScan(scanID, nessie.ExportCSV)
 	if err != nil {
-		return []byte{}, err
+		return nil, err
 	}
+	// Wait for the scan to be finished before exporting.
 	for {
-		if finished, err := nessus.ExportFinished(scanID, exportID); err != nil {
-			return []byte{}, err
-		} else if finished {
+		finished, err := nessus.ExportFinished(scanID, exportID)
+		if err != nil {
+			return nil, err
+		}
+		if finished {
 			break
 		}
 		time.Sleep(5 * time.Second)
@@ -89,113 +100,172 @@ func (bt *Nessusbeat) ExportScanCSV(nessus nessie.Nessus, uuid string) ([]byte, 
 	return nessus.DownloadExport(scanID, exportID)
 }
 
+// Adds a UUID to current processing scan list to prevent multiple scans of the same UUID from occurring.
+func (bt *Nessusbeat) trackUUID(uuid string) bool {
+	bt.mutex.Lock()
+	if bt.scans[uuid] {
+		return false
+	}
+	bt.scans[uuid] = true
+	bt.mutex.Unlock()
+	return true
+}
+
+// Removes a UUID from the currently processing scans.
+func (bt *Nessusbeat) forgetUUID(uuid string) {
+	bt.mutex.Lock()
+	delete(bt.scans, uuid)
+	bt.mutex.Unlock()
+}
+
+// Scans the file at the specified path and emits events based on the nessus scan contents.
+func (bt *Nessusbeat) scan(path string) {
+	// Retrieve UUID from nessus file.
+	basename := filepath.Base(path)
+	uuid := strings.TrimSuffix(basename, filepath.Ext(basename))
+
+	// If UUID is already being scanned, ignore this call.
+	if !bt.trackUUID(uuid) {
+		return
+	}
+	defer bt.forgetUUID(uuid) // Remove UUID after scan is complete.
+	logp.Info("exporting nessus scan '%s'", uuid)
+	var b []byte
+
+	// Try to connect to the nessus API to retrieve the newest scan.
+	err := backoff.RetryNotify(
+		func() error {
+			nessus, err := connectAPI(bt.config.APIUrl, bt.config.CACertPath)
+			if err != nil {
+				return fmt.Errorf("cannot connect to nessus API: %s", err)
+			}
+			if err = nessus.Login(bt.config.APIUsername, bt.config.APIPassword); err != nil {
+				return fmt.Errorf("cannot login to nessus API: %s", err)
+			}
+			defer nessus.Logout()
+			b, err = exportCSVScan(nessus, uuid)
+			if err != nil {
+				return fmt.Errorf("cannot retrieve nessus scan '%s': %s", uuid, err)
+			}
+			return nil
+		},
+		backoff.WithMaxTries(backoff.NewExponentialBackOff(), 5),
+		func(err error, duration time.Duration) {
+			logp.Warn(err.Error())
+			logp.Warn("retrying connection in %s", duration)
+		},
+	)
+	if err != nil {
+		logp.Err(err.Error())
+		return
+	}
+	bt.readCSVScan(b)
+}
+
+// Places an even onto the events channel. This should typically be called with go to prevent the
+// goroutine from blocking.
+func (bt *Nessusbeat) emit(event beat.Event) { bt.events <- event }
+
+// Begins watching for nessus file write events from the given watcher.
+func (bt *Nessusbeat) watch(watcher *fsnotify.Watcher) error {
+	for {
+		select {
+		case event := <-watcher.Events:
+			if event.Op&fsnotify.Write == fsnotify.Write && filepath.Ext(event.Name) == ".nessus" {
+				// Start scan in goroutine in order to consume back to back fsnotify write events.
+				// Otherwise the events will queue until the scan is done, which will create a cascade of
+				// duplicate events.
+				go bt.scan(event.Name)
+			}
+		case err := <-watcher.Errors:
+			logp.Err(err.Error())
+		}
+	}
+}
+
+// Read a nessus scan and emit each record as an event.
+func (bt *Nessusbeat) readCSVScan(result []byte) {
+	r := csv.NewReader(bytes.NewReader(result))
+	_, err := r.Read() // skip header row
+	if err != nil {
+		logp.Err(err.Error())
+		return
+	}
+	for {
+		record, err := r.Read()
+		if err == io.EOF {
+			return
+		}
+		if err != nil {
+			logp.Err(err.Error())
+			return
+		}
+		if len(record) != 13 {
+			logp.Err("invalid field count: expected '13' fields but received '%d", len(record))
+			continue
+		}
+		now := time.Now()
+		event := beat.Event{
+			Timestamp: now,
+			Fields: common.MapStr{
+				"@timestamp":    common.Time(now),
+				"type":          bt.name,
+				"plugin_id":     record[0],
+				"cve":           record[1],
+				"cvss":          record[2],
+				"risk":          record[3],
+				"host":          record[4],
+				"protocol":      record[5],
+				"port":          record[6],
+				"name":          record[7],
+				"synopsis":      record[8],
+				"description":   record[9],
+				"solution":      record[10],
+				"see_also":      record[11],
+				"plugin_output": record[12],
+			},
+		}
+
+		// Place current timestamp on any other configured timestamp fields.
+		for _, field := range strings.Split(bt.config.TimestampFields, ",") {
+			event.Fields[strings.TrimSpace(field)] = now
+		}
+
+		go bt.emit(event) // Emit the event asynchronously to prevent holding up the event channel.
+	}
+}
+
+// Starts the nessus beat.
 func (bt *Nessusbeat) Run(b *beat.Beat) error {
 	logp.Info("nessusbeat is running! Hit CTRL-C to stop it.")
 
-	watcher, err := fsnotify.NewWatcher()
+	// Create watcher with the given report path.
+	watcher, err := watch(bt.config.ReportPath)
 	if err != nil {
 		logp.WTF(err.Error())
 	}
-	defer watcher.Close()
-	err = watcher.Add(bt.config.ReportPath)
-	if err != nil {
-		logp.WTF(err.Error())
-	}
-
 	bt.client, err = b.Publisher.Connect()
-	results := make(chan []byte)
 
-	go func() {
-		for {
-			select {
-			case event := <-watcher.Events:
-				ext := filepath.Ext(event.Name)
-				if event.Op&fsnotify.Write == fsnotify.Write && ext == ".nessus" {
-					basename := filepath.Base(event.Name)
-					uuid := strings.TrimSuffix(basename, filepath.Ext(basename))
-					logp.Info("Exporting scan %s", uuid)
-					var csv []byte
-					err = backoff.RetryNotify(
-						func() error {
-							nessus, err := bt.NewConnection()
-							if err != nil {
-								logp.WTF(err.Error())
-							}
-							if err = bt.Login(*nessus); err != nil {
-								logp.WTF(err.Error())
-							}
-							defer (*nessus).Logout()
-							csv, err = bt.ExportScanCSV(*nessus, uuid)
-							return err
-						},
-						backoff.WithMaxTries(backoff.NewExponentialBackOff(), 5),
-						func(err error, duration time.Duration) {
-							logp.Warn(err.Error())
-							logp.Warn("Retrying in %d", duration)
-						},
-					)
-					if err != nil {
-						logp.Err(err.Error())
-						continue
-					}
-					results <- csv
-				}
-			case err := <-watcher.Errors:
-				logp.Err(err.Error())
-			}
-		}
-	}()
+	// Initialize channels in run step instead of initialization, otherwise if a beat is stopped
+	// then restarted it will
+	bt.done = make(chan struct{})
+	bt.events = make(chan beat.Event)
+
+	go bt.watch(watcher)
 
 	for {
 		select {
 		case <-bt.done:
 			return nil
-		case result := <-results:
-			r := csv.NewReader(bytes.NewReader(result))
-			_, err := r.Read() // skip header row
-			if err != nil {
-				logp.WTF(err.Error())
-			}
-			for {
-				record, err := r.Read()
-				if err == io.EOF {
-					break
-				}
-				if err != nil {
-					logp.WTF(err.Error())
-				}
-				if len(record) != 13 {
-					logp.Err("Invalid number of fields: %d", len(record))
-					continue
-				}
-				
-				event := beat.Event{
-					Timestamp: time.Now(),
-					Fields:  common.MapStr{
-						"@timestamp":    common.Time(time.Now()),
-						"type":          b.Info.Name,
-						"plugin_id":     record[0],
-						"cve":           record[1],
-						"cvss":          record[2],
-						"risk":          record[3],
-						"host":          record[4],
-						"protocol":      record[5],
-						"port":          record[6],
-						"name":          record[7],
-						"synopsis":      record[8],
-						"description":   record[9],
-						"solution":      record[10],
-						"see_also":      record[11],
-						"plugin_output": record[12],
-						},
-					}
-				bt.client.Publish(event)
-			}
+		case event := <-bt.events:
+			bt.client.Publish(event)
 		}
 	}
 }
 
+// Stops the nessus beat.
 func (bt *Nessusbeat) Stop() {
 	bt.client.Close()
 	close(bt.done)
+	close(bt.events)
 }
--- config/config.go
+++ config/config.go
@@ -4,15 +4,15 @@
 package config
 
 type Config struct {
-	CaCertPath        string `config:"cacert_path"`
-	ReportPath        string `config:"report_path"`
-	NessusApiUrl      string `config:"api_url"`
-	NessusApiUsername string `config:"api_username"`
-	NessusApiPassword string `config:"api_password"`
-	TimestampFields   string `config:"timestamp_fields"`
+	CACertPath      string `config:"cacert_path"`
+	ReportPath      string `config:"report_path"`
+	APIUrl          string `config:"api_url"`
+	APIUsername     string `config:"api_username"`
+	APIPassword     string `config:"api_password"`
+	TimestampFields string `config:"timestamp_fields"`
 }
 
 var DefaultConfig = Config{
-	ReportPath:   "/opt/nessus/var/nessus/users/admin/reports",
-	NessusApiUrl: "https://localhost:8834",
+	ReportPath: "/opt/nessus/var/nessus/users/admin/reports",
+	APIUrl:     "https://localhost:8834",
 }
